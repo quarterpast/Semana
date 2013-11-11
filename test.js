4d0ebddd96dd8f92623c04b1d534bd6992d7f88e
Matt Brennan <mattyb1000@gmail.com>
2013-11-11 14:18:25 +0000

    rewrite tests for new api

--- test.js
+++ test.js
@@ -2,27 +2,27 @@ var assert = require('better-assert'),
 getDaysString = require('./index.js');
 
 // single days should just print the day
-assert("Mon" === getDaysString({mon: true}));
-assert("Tue" === getDaysString({tue: true}));
-assert("Wed" === getDaysString({wed: true}));
-assert("Thu" === getDaysString({thu: true}));
-assert("Fri" === getDaysString({fri: true}));
-assert("Sat" === getDaysString({sat: true}));
-assert("Sun" === getDaysString({sun: true}));
+assert("Mon" === getDaysString(['mon']));
+assert("Tue" === getDaysString(['tue']));
+assert("Wed" === getDaysString(['wed']));
+assert("Thu" === getDaysString(['thu']));
+assert("Fri" === getDaysString(['fri']));
+assert("Sat" === getDaysString(['sat']));
+assert("Sun" === getDaysString(['sun']));
 
 // runs should be hyphen'd
-assert("Mon - Wed" === getDaysString({mon: true, tue: true, wed: true}));
-assert("Wed - Thu" === getDaysString({wed: true, thu: true}));
+assert("Mon - Wed" === getDaysString(['mon', 'tue', 'wed']));
+assert("Wed - Thu" === getDaysString(['wed', 'thu']));
 
 // but not across week boundaries
-assert("Mon, Sun" === getDaysString({mon: true, sun: true}));
+assert("Mon, Sun" === getDaysString(['mon', 'sun']));
 
 // weekdays, weekends
-assert("Weekdays" === getDaysString({mon: true, tue: true, wed: true, thu: true, fri: true}));
-assert("Weekends" === getDaysString({sat: true, sun: true}));
+assert("Weekdays" === getDaysString(['mon', 'tue', 'wed', 'thu', 'fri']));
+assert("Weekends" === getDaysString(['sat', 'sun']));
 
 // disjoint runs should be comma'd
-assert("Mon, Thu, Sat" === getDaysString({mon: true, thu: true, sat: true}));
-assert("Mon, Thu - Fri" === getDaysString({mon: true, thu: true, fri: true}));
-assert("Mon - Wed, Sat" === getDaysString({mon: true, tue: true, wed: true, sat: true}));
-assert("Mon - Wed, Weekends" === getDaysString({mon: true, tue: true, wed: true, sat: true, sun: true}));
+assert("Mon, Thu, Sat" === getDaysString(['mon', 'thu', 'sat']));
+assert("Mon, Thu - Fri" === getDaysString(['mon', 'thu', 'fri']));
+assert("Mon - Wed, Sat" === getDaysString(['mon', 'tue', 'wed', 'sat']));
+assert("Mon - Wed, Weekends" === getDaysString(['mon', 'tue', 'wed', 'sat', 'sun']));
