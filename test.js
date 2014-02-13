027612a6d6a817cc5514922ba4e2270e657d2799
Matt Brennan <mattyb1000@gmail.com>
2014-02-13 14:46:22 +0000

    rename function in tests

--- test.js
+++ test.js
@@ -1,35 +1,35 @@
 var assert = require('better-assert'),
-getDaysString = require('./dist/semana.js');
+semana = require('./dist/semana.js');
 
 // single days should just print the day
-assert("Mon" === getDaysString(['mon']));
-assert("Tue" === getDaysString(['tue']));
-assert("Wed" === getDaysString(['wed']));
-assert("Thu" === getDaysString(['thu']));
-assert("Fri" === getDaysString(['fri']));
-assert("Sat" === getDaysString(['sat']));
-assert("Sun" === getDaysString(['sun']));
+assert("Mon" === semana(['mon']));
+assert("Tue" === semana(['tue']));
+assert("Wed" === semana(['wed']));
+assert("Thu" === semana(['thu']));
+assert("Fri" === semana(['fri']));
+assert("Sat" === semana(['sat']));
+assert("Sun" === semana(['sun']));
 
 // runs should be hyphen'd
-assert("Mon - Wed" === getDaysString(['mon', 'tue', 'wed']));
-assert("Wed - Thu" === getDaysString(['wed', 'thu']));
+assert("Mon - Wed" === semana(['mon', 'tue', 'wed']));
+assert("Wed - Thu" === semana(['wed', 'thu']));
 
 // but not across week boundaries
-assert("Mon, Sun" === getDaysString(['mon', 'sun']));
+assert("Mon, Sun" === semana(['mon', 'sun']));
 
 // weekdays, weekends, every day
-assert("Weekdays" === getDaysString(['mon', 'tue', 'wed', 'thu', 'fri']));
-assert("Mon - Sat" === getDaysString(['mon', 'tue', 'wed', 'thu', 'fri', 'sat']));
-assert("Weekdays, Sun" === getDaysString(['mon', 'tue', 'wed', 'thu', 'fri', 'sun']));
-assert("Weekends" === getDaysString(['sat', 'sun']));
-assert("Every day" === getDaysString(['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']));
+assert("Weekdays" === semana(['mon', 'tue', 'wed', 'thu', 'fri']));
+assert("Mon - Sat" === semana(['mon', 'tue', 'wed', 'thu', 'fri', 'sat']));
+assert("Weekdays, Sun" === semana(['mon', 'tue', 'wed', 'thu', 'fri', 'sun']));
+assert("Weekends" === semana(['sat', 'sun']));
+assert("Every day" === semana(['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']));
 
 // disjoint runs should be comma'd
-assert("Mon, Thu, Sat" === getDaysString(['mon', 'thu', 'sat']));
-assert("Mon, Thu - Fri" === getDaysString(['mon', 'thu', 'fri']));
-assert("Mon - Wed, Sat" === getDaysString(['mon', 'tue', 'wed', 'sat']));
-assert("Mon - Wed, Weekends" === getDaysString(['mon', 'tue', 'wed', 'sat', 'sun']));
+assert("Mon, Thu, Sat" === semana(['mon', 'thu', 'sat']));
+assert("Mon, Thu - Fri" === semana(['mon', 'thu', 'fri']));
+assert("Mon - Wed, Sat" === semana(['mon', 'tue', 'wed', 'sat']));
+assert("Mon - Wed, Weekends" === semana(['mon', 'tue', 'wed', 'sat', 'sun']));
 
 // empty
-assert("" === getDaysString([]));
+assert("" === semana([]));
 
