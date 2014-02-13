06ad0a9d94b5e6dd5b69b35067630e8a08ddf6c2
Matt Brennan <mattyb1000@gmail.com>
2014-02-13 14:52:30 +0000

    i18n tests

--- test.js
+++ test.js
@@ -33,3 +33,48 @@ assert("Mon - Wed, Weekends" === semana(['mon', 'tue', 'wed', 'sat', 'sun']));
 // empty
 assert("" === semana([]));
 
+// can create a new lanugage context
+var semanaEs = semana.lang({
+	mon: 'Lu',
+	tue: 'Ma',
+	wed: 'Mx',
+	thu: 'Ju',
+	fri: 'Vi',
+	sat: 'Sa',
+	sun: 'Do',
+	everyday: 'Cada día',
+	weekdays: 'Días de semana',
+	weekend: 'Fin de semana'
+});
+
+// single days should just print the day
+assert("Lu" === semanaEs(['mon']));
+assert("Ma" === semanaEs(['tue']));
+assert("Mx" === semanaEs(['wed']));
+assert("Ju" === semanaEs(['thu']));
+assert("Vi" === semanaEs(['fri']));
+assert("Sa" === semanaEs(['sat']));
+assert("Do" === semanaEs(['sun']));
+
+// runs should be hyphen'd
+assert("Lu - Mx" === semanaEs(['mon', 'tue', 'wed']));
+assert("Mx - Vi" === semanaEs(['wed', 'thu']));
+
+// but not across week boundaries
+assert("Lu, Do" === semanaEs(['mon', 'sun']));
+
+// weekdays, weekends, every day
+assert("Días de semana" === semanaEs(['mon', 'tue', 'wed', 'thu', 'fri']));
+assert("Lu - Sa" === semanaEs(['mon', 'tue', 'wed', 'thu', 'fri', 'sat']));
+assert("Días de semana, Do" === semanaEs(['mon', 'tue', 'wed', 'thu', 'fri', 'sun']));
+assert("Fin de semana" === semanaEs(['sat', 'sun']));
+assert("Cada día" === semanaEs(['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']));
+
+// disjoint runs should be comma'd
+assert("Lu, Ju, Sa" === semanaEs(['mon', 'thu', 'sat']));
+assert("Lu, Ju - Vi" === semanaEs(['mon', 'thu', 'fri']));
+assert("Lu - Mx, Sa" === semanaEs(['mon', 'tue', 'wed', 'sat']));
+assert("Lu - Mx, Fin de semana" === semanaEs(['mon', 'tue', 'wed', 'sat', 'sun']));
+
+// empty
+assert("" === semanaEs([]));
